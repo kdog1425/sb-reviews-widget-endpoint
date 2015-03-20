08a929f4832aa11e1547f5c5302d88e08e36ee1e
Khen Price <kenp332@gmail.com>
2015-03-20 14:31:14 -0400

    added gem to allow cross origin requests, fetching reviews with ajax

--- sb_review_widget/app/controllers/review_widget_controller.rb
+++ sb_review_widget/app/controllers/review_widget_controller.rb
@@ -1,6 +1,7 @@
 class ReviewWidgetController < ApplicationController
 
   layout 'application'
+  respond_to :html, :xml, :json
 
   def index
   	render('basic')
@@ -27,6 +28,33 @@ class ReviewWidgetController < ApplicationController
 	    "name": "Brian J."
 	  }}
 	]
+  end
+
+  def fetch_reviews
+  	@reviews = [{
+	  "name": "Marshall Mixing", 
+	  "body": "Marshall did a great job turning a 'tin-like' sound into a warm earthy sound on a song I had recorded. Thanks!", 
+	  "rating": "4", 
+	  "reviewer": {
+	    "name": "Mary Jane Marcus"
+	  }},{
+	  "name": "Marshall Mixing", 
+	  "body": "Marshall is my favorite pro out there. Thanks!", 
+	  "rating": "5", 
+	  "reviewer": {
+	    "name": "Joe Schmoe"
+	  }},{
+	  "name": "Marshall Mixing", 
+	  "body": "Marshall is without question the best singer I've ever worked with, a true artist and a gentleman. Couldn't recommend him enough!!", 
+	  "rating": "4", 
+	  "reviewer": {
+	    "name": "Brian J."
+	  }}
+	]
+	respond_to do |format|
+		format.html # show.html.erb
+		format.json { render json: @reviews }
+	end
   end
 
   def not_authenticated
