c4fc41467847c79cd9e84ee756ffb215096ffd2d
Khen Price <khen@humtap.com>
2016-01-14 19:04:21 -0500

    removed -basic- code

--- app/controllers/review_widget_controller.rb
+++ app/controllers/review_widget_controller.rb
@@ -8,26 +8,7 @@ class ReviewWidgetController < ApplicationController
   end
 
   def basic
-  	@reviews = [{
-	  "name": "Marshall Mixing", 
-	  "body": "Marshall did a great job turning a 'tin-like' sound into a warm earthy sound on a song I had recorded. Thanks!", 
-	  "rating": "4", 
-	  "reviewer": {
-	    "name": "Mary Jane Marcus"
-	  }},{
-	  "name": "Marshall Mixing", 
-	  "body": "Marshall is my favorite pro out there. Thanks!", 
-	  "rating": "5", 
-	  "reviewer": {
-	    "name": "Joe Schmoe"
-	  }},{
-	  "name": "Marshall Mixing", 
-	  "body": "Marshall is without question the best singer I've ever worked with, a true artist and a gentleman. Couldn't recommend him enough!!", 
-	  "rating": "4", 
-	  "reviewer": {
-	    "name": "Brian J."
-	  }}
-	]
+  	
   end
 
   def fetch_reviews
