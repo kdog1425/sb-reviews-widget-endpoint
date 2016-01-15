ee1fd36f2464a60532acdbce7ae8e93b024a52df
Khen Price <khen@humtap.com>
2016-01-14 19:24:22 -0500

    further fixes to mock data

--- app/controllers/review_widget_controller.rb
+++ app/controllers/review_widget_controller.rb
@@ -8,30 +8,30 @@ class ReviewWidgetController < ApplicationController
   end
 
   def basic
-  	
   end
 
   def fetch_reviews
-  	@reviews = [{
-	  "name": "Marshall Mixing", 
-	  "body": "Marshall did a great job turning a 'tin-like' sound into a warm earthy sound on a song I had recorded. Thanks!", 
-	  "rating": "4", 
-	  "reviewer": {
-	    "name": "Mary Jane Marcus"
-	  }},{
-	  "name": "Marshall Mixing", 
-	  "body": "Marshall is my favorite pro out there. Thanks!", 
-	  "rating": "5", 
-	  "reviewer": {
-	    "name": "Joe Schmoe"
-	  }},{
-	  "name": "Marshall Mixing", 
-	  "body": "Marshall is without question the best singer I've ever worked with, a true artist and a gentleman. Couldn't recommend him enough!!", 
-	  "rating": "4", 
-	  "reviewer": {
-	    "name": "Brian J."
-	  }}
-	]
+  	
+  	review1 = {:name => "Marshall Mixing",
+  	  :body => "Marshall did a great job turning a 'tin-like' sound into a warm earthy sound on a song I had recorded. Thanks!", 
+	  :rating => "4", 
+	  :reviewer => {:name => "Mary Jane Marcus"}
+  	}
+
+  	review2 = {:name => "Marshall Mixing",
+  	  :body => "Marshall is my favorite pro out there. Thanks!",
+	  :rating => "5", 
+	  :reviewer => {:name => "Joe Schmoe"}
+  	}
+
+  	review3 = {:name => "Marshall Mixing",
+  	  :body => "Marshall is without question the best singer I've ever worked with, a true artist and a gentleman. Couldn't recommend him enough!!", 
+	  :rating => "4", 
+	  :reviewer => {:name => "Brian F."}
+  	}
+
+  	@reviews = [review1 , review2, review3]
+
 	respond_to do |format|
 		format.html # show.html.erb
 		format.json { render json: @reviews }
