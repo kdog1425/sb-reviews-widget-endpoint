e8aa5b395a7c25cafd03901356a34d12fa303bb6
Khen Price <khen@humtap.com>
2016-01-14 19:26:19 -0500

    further fixes to mock data

--- app/controllers/review_widget_controller.rb
+++ app/controllers/review_widget_controller.rb
@@ -8,6 +8,25 @@ class ReviewWidgetController < ApplicationController
   end
 
   def basic
+  	review1 = {:name => "Marshall Mixing",
+  	  :body => "Marshall did a great job turning a 'tin-like' sound into a warm earthy sound on a song I had recorded. Thanks!", 
+	  :rating => "4", 
+	  :reviewer => {:name => "Mary Jane Marcus"}
+  	}
+
+  	review2 = {:name => "Marshall Mixing",
+  	  :body => "Marshall is my favorite pro out there. Thanks!",
+	  :rating => "5", 
+	  :reviewer => {:name => "Joe Schmoe"}
+  	}
+
+  	review3 = {:name => "Marshall Mixing",
+  	  :body => "Marshall is without question the best singer I've ever worked with, a true artist and a gentleman. Couldn't recommend him enough!!", 
+	  :rating => "4", 
+	  :reviewer => {:name => "Brian F."}
+  	}
+
+  	@reviews = [review1 , review2, review3]
   end
 
   def fetch_reviews
